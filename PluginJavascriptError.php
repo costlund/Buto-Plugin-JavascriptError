2b92b0ceb21c15b088764978489b43783fb731f5
Claes Östlund <claes@stenbergit.se>
2020-02-04 12:49:26 +0100

    Add manifest and readme

--- PluginJavascriptError.php
+++ PluginJavascriptError.php
@@ -1,6 +1,14 @@
 <?php
 class PluginJavascriptError{
   public function widget_include($data){
+    /**
+     * 
+     */
+    wfPlugin::includeonce('wf/embed');
+    wfPlugin::enable('wf/embed');
+    /**
+     * 
+     */
     $widget = new PluginWfYml(__DIR__.'/widget/include.yml');
     wfDocument::renderElement($widget->get());
   }
---
+++ README.md
@@ -0,0 +1,14 @@
+# Buto-Plugin-JavascriptError
+
+Catch Javascript errors via window.onerror. Show an alert.
+
+## Widget include
+
+Include in head or first in body.
+
+```
+type: widget
+data:
+  plugin: javascript/error
+  method: include
+```
